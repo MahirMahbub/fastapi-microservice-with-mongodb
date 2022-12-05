5f18017d7a162f9f6b9faff2d4ad29cb262c22ad
Mahir Mahbub <mahir.mahbub@ixorasolution.com>
2022-12-05 15:44:14 +0600

    add education create and add by admin

--- skill_management/controllers/education.py
+++ skill_management/controllers/education.py
@@ -1,11 +1,13 @@
+import uuid
+
 from fastapi import APIRouter, Request, Depends, Body
 from fastapi.responses import ORJSONResponse
 
 from skill_management.schemas.base import ErrorMessage
-from skill_management.schemas.education import EducationCreateRequest, EducationCreateResponse, \
-    EducationListDataResponse
+from skill_management.schemas.education import EducationCreateRequest, EducationListDataResponse, \
+    EducationCreateAdminRequest
 from skill_management.services.education import EducationService
-from skill_management.utils.auth_manager import JWTBearer
+from skill_management.utils.auth_manager import JWTBearer, JWTBearerAdmin
 from skill_management.utils.logger import get_logger
 from skill_management.utils.profile_manager import get_profile_email
 
@@ -27,41 +29,42 @@ logger = get_logger()
                            },
                        },
                        )
-async def create_education(request: Request,  # type: ignore
-                           education: EducationCreateRequest = Body(
-                               examples={
-                                   "CREATE": {
-                                       "summary": "Create Body",
-                                       "description": "a example of body for create operation",
-                                       "value":
-                                           {
-                                               "degree_name": "B.Sc in Computer Science",
-                                               "school_name": "University of Dhaka",
-                                               "passing_year": "2019",
-                                               "grade": 3.80
-                                           }
-                                   },
+async def create_education_by_user(request: Request,  # type: ignore
+                                   education_request: EducationCreateRequest = Body(
+                                       examples={
+                                           "CREATE": {
+                                               "summary": "Create Body",
+                                               "description": "a example of body for create operation",
+                                               "value":
+                                                   {
+                                                       "degree_name": "B.Sc in Computer Science",
+                                                       "school_name": "University of Dhaka",
+                                                       "passing_year": "2019",
+                                                       "grade": 3.80,
+                                                       "status": 1
+                                                   }
+                                           },
 
-                                   "UPDATE":
-                                       {
-                                           "summary": "Update Body",
-                                           "description": "a example of body for update operation",
-                                           "value":
+                                           "UPDATE":
                                                {
-                                                   "education_id": 1,
-                                                   "degree_name": "B.Sc in Computer Science",
-                                                   "school_name": "University of Dhaka",
-                                                   "passing_year": "2019",
-                                                   "grade": 3.80,
-                                                   "status": 1
-                                               },
-                                       }
+                                                   "summary": "Update Body",
+                                                   "description": "a example of body for update operation",
+                                                   "value":
+                                                       {
+                                                           "education_id": 1,
+                                                           "degree_name": "B.Sc in Computer Science",
+                                                           "school_name": "University of Dhaka",
+                                                           "passing_year": "2019",
+                                                           "grade": 3.80,
+                                                           "status": 1
+                                                       },
+                                               }
 
-                               },
+                                       },
 
-                               description="input education data"),
-                           user_id: str = Depends(JWTBearer()),
-                           service: EducationService = Depends()):
+                                       description="input education data"),
+                                   user_id: str = Depends(JWTBearer()),
+                                   service: EducationService = Depends()):
     """
     **Create:** Must provide all the data except *"education_id"*. *"status"* is optional.
     
@@ -70,5 +73,68 @@ async def create_education(request: Request,  # type: ignore
     """
 
     email = await get_profile_email(user_id=user_id, request=request)
-    return await service.create_or_update_education_by_user(education_request=education,
-                                                      email=email)
+    return await service.create_or_update_education_by_user(education_request=education_request,
+                                                            email=email)
+
+
+@education_router.post("/admin/profile/educations",
+                       response_class=ORJSONResponse,
+                       response_model=EducationListDataResponse,
+                       status_code=201,
+                       responses={
+                           400: {
+                               "model": ErrorMessage,
+                               "description": "The education is not created"
+                           },
+                           201: {
+                               "description": "The education is successfully created",
+                           },
+                       },
+                       )
+async def create_education_by_admin(request: Request,  # type: ignore
+                                    education_request: EducationCreateAdminRequest = Body(
+                                       examples={
+                                           "CREATE": {
+                                               "summary": "Create Body",
+                                               "description": "a example of body for create operation",
+                                               "value":
+                                                   {
+                                                       "profile_id": uuid.uuid4(),
+                                                       "degree_name": "B.Sc in Computer Science",
+                                                       "school_name": "University of Dhaka",
+                                                       "passing_year": "2019",
+                                                       "grade": 3.80,
+                                                       "status": 1
+                                                   }
+                                           },
+
+                                           "UPDATE":
+                                               {
+                                                   "summary": "Update Body",
+                                                   "description": "a example of body for update operation",
+                                                   "value":
+                                                       {
+                                                           "profile_id": uuid.uuid4(),
+                                                           "education_id": 1,
+                                                           "degree_name": "B.Sc in Computer Science",
+                                                           "school_name": "University of Dhaka",
+                                                           "passing_year": "2019",
+                                                           "grade": 3.80,
+                                                           "status": 1
+                                                       },
+                                               }
+
+                                       },
+
+                                       description="input education data"),
+                                    user_id: str = Depends(JWTBearerAdmin()),
+                                    service: EducationService = Depends()):
+    """
+    **Create:** Must provide all the data except *"education_id"*. *"status"* is optional.
+
+
+    **Update:** Must provide *"education_id"*. Other attributes are optional.
+    """
+
+    email = await get_profile_email(user_id=user_id, request=request)
+    return await service.create_or_update_education_by_admin(education_request=education_request)
--- skill_management/services/education.py
+++ skill_management/services/education.py
@@ -7,13 +7,13 @@ from skill_management.enums import StatusEnum
 from skill_management.models.profile import Profiles
 from skill_management.repositories.profile import ProfileRepository
 from skill_management.schemas.education import EducationCreateRequest, EducationCreateResponse, ProfileEducation, \
-    EducationListDataResponse
+    EducationListDataResponse, EducationCreateAdminRequest
 from skill_management.schemas.profile import ProfileEducationView
 
 
 class EducationService:
     async def create_or_update_education_by_user(self, education_request: EducationCreateRequest,
-                                                 email: str| None) -> EducationListDataResponse:
+                                                 email: str | None) -> EducationListDataResponse:
         if email is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                 detail="Authentication failed")
@@ -27,6 +27,20 @@ class EducationService:
             It is a create operation
             """
             return await self._create_education_by_user(education_request, email)
+
+    async def create_or_update_education_by_admin(self,
+                                                  education_request: EducationCreateAdminRequest) -> EducationListDataResponse:
+
+        if education_request.education_id is not None:
+            """
+            It is an update operation
+            """
+            return await self._update_education_by_admin(education_request)
+        else:
+            """
+            It is a create operation
+            """
+            return await self._create_education_by_admin(education_request)
 
     async def _update_education_by_user(self, education_request: EducationCreateRequest,
                                         email: str) -> EducationListDataResponse:
@@ -71,12 +85,12 @@ class EducationService:
                     degree_name=education.degree_name,
                     school_name=education.school_name,
                     passing_year=education.passing_year,
-                    grade=education.grade
+                    grade=education.grade,
+                    status=education.status,
                 )
                 for education in db_profile.educations
             ]
         )
-
 
     async def _create_education_by_user(self, education_request: EducationCreateRequest,
                                         email: str) -> EducationListDataResponse:
@@ -113,7 +127,7 @@ class EducationService:
             school_name=education_request.school_name,
             status=cast(StatusEnum, education_request.status)
         )
-        db_profile: Profiles = await profile_crud_manager.update_by_query(
+        db_profile: Profiles = cast(Profiles, await profile_crud_manager.update_by_query(
             query={
                 "_id": profile_educations.id
             },
@@ -123,6 +137,7 @@ class EducationService:
                 )
             }
         )
+                                    )
         return EducationListDataResponse(
             educations=[
                 EducationCreateResponse(
@@ -130,7 +145,118 @@ class EducationService:
                     degree_name=education.degree_name,
                     school_name=education.school_name,
                     passing_year=education.passing_year,
-                    grade=education.grade
+                    grade=education.grade,
+                    status=education.status,
+                )
+                for education in db_profile.educations
+            ]
+        )
+
+    async def _update_education_by_admin(self,
+                                         education_request: EducationCreateAdminRequest) -> EducationListDataResponse:
+        profile_crud_manager = ProfileRepository()
+        profile_educations = cast(
+            ProfileEducationView,
+            await profile_crud_manager.get_by_query(
+                query={
+                    "_id": education_request.profile_id
+                },
+                projection_model=ProfileEducationView
+            )
+        )
+        if profile_educations is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="You are not allowed to update the user profile")
+        has_education = await Profiles.find(
+            {
+                "_id": education_request.profile_id
+            },
+            ElemMatch(Profiles.educations, {"education_id": education_request.education_id}),
+            projection_model=ProfileEducationView).first_or_none()
+        if has_education is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="Must provide a valid education id to update")
+        education_request_dict = education_request.dict(exclude_unset=True, exclude_defaults=True)
+        education_request_dict.pop("education_id")
+        education_request_dict = {
+            "educations.$." + str(key): val for key, val in education_request_dict.items()
+        }
+        db_profile: Profiles = await profile_crud_manager.update_by_query(  # type: ignore
+            query={
+                "educations.education_id": education_request.education_id,
+                "_id": profile_educations.id
+            },
+            item_dict=education_request_dict
+        )
+        return EducationListDataResponse(
+            educations=[
+                EducationCreateResponse(
+                    education_id=education.education_id,
+                    degree_name=education.degree_name,
+                    school_name=education.school_name,
+                    passing_year=education.passing_year,
+                    grade=education.grade,
+                    status=education.status,
+                )
+                for education in db_profile.educations
+            ]
+        )
+
+    async def _create_education_by_admin(self,
+                                         education_request: EducationCreateAdminRequest) -> EducationListDataResponse:
+        profile_crud_manager = ProfileRepository()
+        profile_educations = cast(
+            ProfileEducationView,
+            await profile_crud_manager.get_by_query(
+                query={
+                    "_id": education_request.profile_id
+                },
+                projection_model=ProfileEducationView
+            )
+        )
+        if profile_educations is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="You are not allowed to update the user profile")
+
+        all_education_ids = [
+            education.education_id for education in profile_educations.educations
+        ]
+        if not all_education_ids:
+            new_education_id = 1
+
+        else:
+            """
+            Calculate new experience id
+            """
+            new_education_id = max(all_education_ids) + 1
+        new_education = ProfileEducation(
+            education_id=new_education_id,
+            degree_name=education_request.degree_name,
+            grade=education_request.grade,
+            passing_year=education_request.passing_year,
+            school_name=education_request.school_name,
+            status=education_request.status
+        )
+        db_profile: Profiles = cast(Profiles, await profile_crud_manager.update_by_query(
+            query={
+                "_id": profile_educations.id
+            },
+            push_item={
+                "educations": new_education.dict(
+                    exclude_unset=True, exclude_none=True
+                )
+            }
+        )
+                                    )
+        return EducationListDataResponse(
+            educations=[
+                EducationCreateResponse(
+                    education_id=education.education_id,
+                    degree_name=education.degree_name,
+                    school_name=education.school_name,
+                    passing_year=education.passing_year,
+                    grade=education.grade,
+                    status=education.status,
                 )
                 for education in db_profile.educations
             ]
